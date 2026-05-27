360fc2528e569a1c4c02318825423fc77760a176
milkCTL <mini_1210@qq.com>
2026-05-28 00:08:01 +0800

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+#include "include/gui.h"
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    InitLauncherUI();
+    return 0;
+}
